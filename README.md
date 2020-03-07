d63faa39a47f26069a47a59ec99a82dd80ac8824
Saurel <okano.public12@gmail.com>
2020-03-07 23:07:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# XcodeGen SampleProject
+
+## 環境構築
+
+### [Mint](https://github.com/yonaskolb/mint)
+```
+mint install yonaskolb/xcodegen
+```
+
+
+## [XcodeGen](https://github.com/yonaskolb/XcodeGen)
+
+`project.yml`を作成する
+
+```
+touch project.yml
+```
+
+XcodeGenでprojectを作成
+```
+mint run xcodegen
+```
---
+++ XcodeGenSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeGenSample
+//
+//  Created by okano on 2020/03/07.
+//  Copyright © 2020 lcr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
